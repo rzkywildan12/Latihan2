7b72a725afac1f05c804712297104eec13d8b882
rzkywildan12 <w1ld4n.r.w1j4y4@gmail.com>
2025-03-15 19:53:48 +0700

    Create Cek bilangan ganjil atau genap3.ts

---
+++ Cek bilangan ganjil atau genap3.ts
@@ -0,0 +1,7 @@
+// 3. Cek bilangan ganjil atau genap
+function isOdd(n: number): boolean {
+    return n % 2 !== 0;
+}
+
+console.log(isOdd(1001)); // Output: true
+console.log(isOdd(1000)); // Output: false

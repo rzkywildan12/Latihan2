b71014b85a0f39e29a4f5eeeac4cfc2267f30857
rzkywildan12 <w1ld4n.r.w1j4y4@gmail.com>
2025-03-15 19:51:04 +0700

    Create Konversi Fahrenheit ke celcius 1.ts

---
+++ Konversi Fahrenheit ke celcius 1.ts
@@ -0,0 +1,5 @@
+// 1. Konversi Fahrenheit ke celcius 
+function fahrenheitToCelcius(fahrenheit: number): number {
+    return (fahrenheit - 32) * 5 / 9;
+}
+console.log(fahrenheitToCelcius(100)); // Output: 37.777...

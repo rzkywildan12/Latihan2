9ea8743f9196abcf926180ee510865b1c31a0147
rzkywildan12 <w1ld4n.r.w1j4y4@gmail.com>
2025-03-15 19:54:51 +0700

    Create Cek apakah sebuah string adalah palindrome5.ts

---
+++ Cek apakah sebuah string adalah palindrome5.ts
@@ -0,0 +1,8 @@
+// 5. Cek apakah sebuah string adalah palindrome
+function isPalindrome(str: string): boolean {
+    let reversed = str.split('').reverse().join('');
+    return str === reversed;
+}
+
+console.log(isPalindrome("madam")); // Output: true
+console.log(isPalindrome("hello")); // Output: false
